803dd95276bb6e6d934e75164b29b8e1d99d0dba
Dorcas Egbende <dorcasegbende@gmail.com>
2024-06-07 08:53:09 +0100

    stylepage

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const articles = require("./data/db.json");
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
+app.use(express.static('public'));
+
 app.get("/", (req, res) => {
   res.render("index");
 });
